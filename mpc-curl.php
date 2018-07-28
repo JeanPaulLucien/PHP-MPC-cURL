3b45c837135f130be12b906c6357a69c398781f0
JeanPaulLucien <paulpub@ya.ru>
2018-07-28 18:40:07 +0300

    Update mpc-curl.php

--- mpc-curl.php
+++ mpc-curl.php
@@ -3,12 +3,12 @@
 
 // Use pattern below to give parameters to function
 $func_cURL_data_dummy = ['url' => null, 
-                        'useragent' => null, 
-						'timeout' => null, 
-						'followlocation' => null, 
-						'ssl_verifyhost' => null, 
-						'ssl_verifypeer' => null, 
-						'returntransfer' => null ];
+						 'useragent' => null,
+						 'timeout' => null,
+						 'followlocation' => null,
+						 'ssl_verifyhost' => null,
+						 'ssl_verifypeer' => null,
+						 'returntransfer' => null ];
 unset($func_cURL_data_dummy);
 // Pattern end
             
@@ -18,7 +18,7 @@ unset($func_cURL_data_dummy);
 // The function
 function cURL($data)
 {
-	$default = ['useragent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0', 
+	$default = ['useragent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0',
 				'timeout' => 60,
 				'followlocation' => 0,
 				'ssl_verifyhost' => 0,
